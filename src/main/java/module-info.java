f66e4cb9c16474ffc0ecd66d13dfe01b370766de
IsDilanZ <dilanismael62@gmail.com>
2024-05-28 19:19:39 -0500

    1st commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.repo5_orderarray2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.repo5_orderarray2 to javafx.fxml;
+    exports com.example.repo5_orderarray2;
+}
