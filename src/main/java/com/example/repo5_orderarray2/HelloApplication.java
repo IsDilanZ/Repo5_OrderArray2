70613a77ee158e179e3c7ea7a0803cf18dfe5d6f
IsDilanZ <dilanismael62@gmail.com>
2024-05-28 22:19:42 -0500

    2rd commit

--- src/main/java/com/example/repo5_orderarray2/HelloApplication.java
+++ src/main/java/com/example/repo5_orderarray2/HelloApplication.java
@@ -10,14 +10,29 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+
+        char[] LettersArray = {'z', 'b', 'e', 'f', 'a', 'c', 'o'};
+
+        int lengthArray = LettersArray.length;
+
+        // Loop to sort array using bubble method
+        for (int i = 0; i < lengthArray - 1; i++) {
+            for (int j = 0; j < lengthArray - 1; j++) {
+                if (LettersArray[j] > LettersArray[j + 1]) {
+                    int temp = LettersArray[j];
+                    LettersArray[j] = LettersArray[j + 1];
+                    LettersArray[j + 1] = (char) temp;
+                }
+            }
+        }
+
+        // print ordered array
+        System.out.println("Ordered array: ");
+        for (int i = 0 ; i<lengthArray ; i++) {
+            System.out.print(LettersArray[i] + " ");
+        }
     }
 }
