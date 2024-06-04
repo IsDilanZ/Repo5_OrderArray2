011363601ba626a0a3c8c2b7aa392005d387ee28
IsDilanZ <dilanismael62@gmail.com>
2024-06-04 17:14:32 -0500

    3rd commit

--- src/main/java/com/example/repo5_orderarray2/HelloApplication.java
+++ src/main/java/com/example/repo5_orderarray2/HelloApplication.java
@@ -14,10 +14,11 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
 
-        char[] LettersArray = {'z', 'b', 'e', 'f', 'a', 'c', 'o'};
+        char[] LettersArray = {'z', 'b', 'e', 'f', 'a', 'c', 'o'}; //Array defining and initialization
 
-        int lengthArray = LettersArray.length;
+        int lengthArray = LettersArray.length; //Assigns the number of the NumberArray's elements to the lengthArray variable
 
+        /* The "lengthArray" variable will be used to establish a superior limit in the next for cycle */
         // Loop to sort array using bubble method
         for (int i = 0; i < lengthArray - 1; i++) {
             for (int j = 0; j < lengthArray - 1; j++) {
@@ -25,6 +26,7 @@ public class HelloApplication extends Application {
                     int temp = LettersArray[j];
                     LettersArray[j] = LettersArray[j + 1];
                     LettersArray[j + 1] = (char) temp;
+                    /* The basic idea is the iterative positioning of new holes (so-called “bubbles”) into the present structure of the component */
                 }
             }
         }
